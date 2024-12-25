af818aa440f17b2b2de413814f566e91827ab6a5
Shivamj237 <87420920+Shivamj237@users.noreply.github.com>
2024-12-25 22:01:51 +0530

    Create operations.sh

---
+++ operations.sh
@@ -0,0 +1,12 @@
+#sum, difference, product, quotient
+read -p "enter first num " x
+read -p "enter second num " y
+
+let sum=$x+$y
+echo "$sum"
+
+let diff=$x-$y
+echo "$diff"
+
+let qu=$x/$y
+echp "$qu"
